b1e484813f924a11bb9bb15d0ae83beb0889c691
raccoon-coding <minsungt@naver.com>
2025-09-26 11:46:03 +0900

    [DEVELOP] Spring 프로젝트 생성

---
+++ src/main/java/MeshX/HypeLink/HypeLinkApplication.java
@@ -0,0 +1,13 @@
+package MeshX.HypeLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypeLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypeLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MeshX/HypeLink/HypeLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package MeshX.HypeLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypeLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
