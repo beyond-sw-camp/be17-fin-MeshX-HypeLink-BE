5de6e27570cf2d64d9e24e34e6134409dc914dd6
David9733 <dltldnr2563@naver.com>
2025-10-20 11:58:14 +0900

    DEV order, notice, promotion 보충

--- src/main/java/MeshX/HypeLink/head_office/notice/model/dto/response/NoticeInfoRes.java
+++ src/main/java/MeshX/HypeLink/head_office/notice/model/dto/response/NoticeInfoRes.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class NoticeInfoRes {
     private Integer id;
@@ -12,24 +14,30 @@ public class NoticeInfoRes {
     private String contents;
     private String author;
     private Boolean isOpen;
+    private LocalDateTime date;
 
     public static NoticeInfoRes toDto(Notice entity) {
+        LocalDateTime displayDate = entity.getUpdatedAt() != null
+                ? entity.getUpdatedAt()
+                : entity.getCreatedAt();
         return NoticeInfoRes.builder()
                 .id(entity.getId())
                 .title(entity.getTitle())
                 .contents(entity.getContents())
                 .author(entity.getAuthor())
                 .isOpen(entity.getIsOpen())
+                .date(displayDate)
                 .build();
     }
 
     @Builder
-    private NoticeInfoRes(String title, String contents, Boolean isOpen, String author, Integer id) {
+    private NoticeInfoRes(String title, String contents, Boolean isOpen, String author, Integer id,  LocalDateTime date) {
         this.id = id;
         this.title = title;
         this.contents = contents;
         this.author = author;
         this.isOpen = isOpen;
+        this.date = date;
     }
     public static Page<NoticeInfoRes> toDtoPage(Page<Notice> page) { return page.map(NoticeInfoRes::toDto);}
 }
