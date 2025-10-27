cae78db53f6c26f875cdc37f9d43978e9bd03771
David9733 <dltldnr2563@naver.com>
2025-10-27 09:35:34 +0900

    DEV Promotion 공지느낌으로 개발완료

--- src/main/java/MeshX/HypeLink/head_office/promotion/model/dto/response/PromotionInfoRes.java
+++ src/main/java/MeshX/HypeLink/head_office/promotion/model/dto/response/PromotionInfoRes.java
@@ -1,99 +1,64 @@
 package MeshX.HypeLink.head_office.promotion.model.dto.response;
 
-import MeshX.HypeLink.head_office.promotion.model.entity.ItemCategory;
+import MeshX.HypeLink.head_office.coupon.model.entity.Coupon;
 import MeshX.HypeLink.head_office.promotion.model.entity.Promotion;
 import MeshX.HypeLink.head_office.promotion.model.entity.PromotionStatus;
-import MeshX.HypeLink.head_office.promotion.model.entity.PromotionType;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Getter
 public class PromotionInfoRes {
-    private PromotionType promotionType;
     private Integer id;
     private String title;
     private String contents;
-    private Double discountRate;    // 할인율
     private LocalDate startDate;    // 할인 시작 시정
     private LocalDate endDate;      // 할인 종료 시점
     private PromotionStatus status;
 
-    private ItemCategory category; // CATEGORY 전용
-    private List<Integer> storeIds;     // STORE 전용
-    private Integer itemId;             // PRODUCT 전용-
+    private String couponType;
+    private String couponName;
+    private Integer couponId;
 
     public static PromotionInfoRes toDto(Promotion entity) {
-        PromotionInfoResBuilder builder = PromotionInfoRes.builder()
+        Coupon coupon = entity.getCoupon();
+
+        return PromotionInfoRes.builder()
                 .id(entity.getId())
-                .promotionType(entity.getPromotionType())
                 .title(entity.getTitle())
                 .contents(entity.getContents())
-                .discountRate(entity.getDiscountRate())
                 .startDate(entity.getStartDate())
                 .endDate(entity.getEndDate())
-                .status(entity.getStatus());
-
-        // 타입별 매핑 분기
-        switch (entity.getPromotionType()) {
-            case STORE -> {
-                List<Integer> storeIds = entity.getPromotionStore().stream()
-                        .map(link -> link.getStore().getId())
-                        .toList();
-                builder.storeIds(storeIds);
-            }
-
-            case PRODUCT -> {
-                entity.getPromotionItems().stream().findFirst().ifPresent(link -> {
-                    builder.itemId(link.getItem().getId());
-                }); //하나만 뽑음
-            }
-
-            case CATEGORY -> {
-                entity.getPromotionCategories().stream().findFirst().ifPresent(link -> {
-                    builder.category(link.getCategory());
-                }); //하나만 뽑음
-            }
-
-            case ALL -> {
-//                // ✅ ALL도 본사(store_id = 1)가 PromotionStore에 연결되어 있으므로 동일하게 처리
-//                List<Integer> storeIds = entity.getPromotionStore().stream()
-//                        .map(link -> link.getStore().getId())
-//                        .toList();
-//                builder.storeIds(storeIds);
-                builder.storeIds(List.of(1));
-            }
-        }
-
-        return builder.build();
+                .status(entity.getStatus())
+                .couponType(coupon.getCouponType().name())
+                .couponName(coupon.getName())
+                .couponId(coupon.getId())
+                .build();
     }
 
      @Builder
-    private PromotionInfoRes(PromotionType promotionType,
+    private PromotionInfoRes(
                 Integer id,
                 String title,
                 String contents,
-                Double discountRate,
                 LocalDate startDate,
                 LocalDate endDate,
                 PromotionStatus status,
-                ItemCategory category,
-                List<Integer> storeIds,
-                Integer itemId) {
-            this.promotionType = promotionType;
+                String couponType
+                ,String couponName
+                , Integer couponId
+                ) {
             this.id = id;
             this.title = title;
             this.contents = contents;
-            this.discountRate = discountRate;
             this.startDate = startDate;
             this.endDate = endDate;
             this.status = status;
-            this.category = category;
-            this.storeIds = storeIds;
-            this.itemId = itemId;
+            this.couponType = couponType;
+            this.couponName = couponName;
+            this.couponId = couponId;
         }
 
     public static Page<PromotionInfoRes> toDtoPage(Page<Promotion>  page) {
--- src/main/java/MeshX/HypeLink/head_office/promotion/service/PromotionService.java
+++ src/main/java/MeshX/HypeLink/head_office/promotion/service/PromotionService.java
@@ -2,18 +2,15 @@ package MeshX.HypeLink.head_office.promotion.service;
 
 
 import MeshX.HypeLink.auth.model.entity.Store;
-import MeshX.HypeLink.auth.repository.StoreJpaRepositoryVerify;
 import MeshX.HypeLink.common.Page.PageRes;
 import MeshX.HypeLink.direct_store.item.model.entity.StoreItem;
-import MeshX.HypeLink.direct_store.item.repository.StoreItemJpaRepositoryVerify;
+import MeshX.HypeLink.head_office.coupon.model.entity.Coupon;
+import MeshX.HypeLink.head_office.coupon.repository.CouponJpaRepositoryVerify;
 import MeshX.HypeLink.head_office.promotion.model.dto.request.PromotionCreateReq;
 import MeshX.HypeLink.head_office.promotion.model.dto.response.PromotionInfoListRes;
 import MeshX.HypeLink.head_office.promotion.model.dto.response.PromotionInfoRes;
 import MeshX.HypeLink.head_office.promotion.model.entity.*;
-import MeshX.HypeLink.head_office.promotion.repository.PromotionCategoryJpaRepositoryVerify;
-import MeshX.HypeLink.head_office.promotion.repository.PromotionItemJpaRepositoryVerify;
 import MeshX.HypeLink.head_office.promotion.repository.PromotionJpaRepositoryVerify;
-import MeshX.HypeLink.head_office.promotion.repository.PromotionStoreJpaRepositoryVerify;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,72 +19,22 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
+
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PromotionService {
-    private final StoreItemJpaRepositoryVerify storeItemRepository;
-    private final StoreJpaRepositoryVerify storeRepository;
     private final PromotionJpaRepositoryVerify repository;
-    private final PromotionStoreJpaRepositoryVerify promotionStoreRepository;
-    private final PromotionItemJpaRepositoryVerify promotionItemRepository;
-    private final PromotionCategoryJpaRepositoryVerify promotionCategoryRepository;
+    private final CouponJpaRepositoryVerify couponRepository;
 
 
     @Transactional
     public void createPromotion(PromotionCreateReq dto) {
-        // 1️⃣ 프로모션 기본 생성
-        Promotion promotion = dto.toEntity();
+        Coupon coupon = couponRepository.findById(dto.getCouponId());
+        Promotion promotion = dto.toEntity(coupon);
         promotion.autoUpdateStatus();
         repository.createPromotion(promotion);
-
-        if (dto.getPromotionType() == PromotionType.PRODUCT && dto.getItemId() != null) {
-            StoreItem item = storeItemRepository.findById(dto.getItemId()); // ✅ 이미 검증된 조회
-            PromotionItem link = PromotionItem.builder()
-                    .promotion(promotion)
-                    .item(item)
-                    .build();
-            promotionItemRepository.save(link);
-        }
-
-        // 2️⃣ 매장 이벤트일 때 PromotionStore 저장
-        if (dto.getPromotionType() == PromotionType.STORE && dto.getStoreIds() != null) {
-            List<PromotionStore> promotionStores = dto.getStoreIds().stream()
-//                    .map(storeId -> {
-//                        Store store = Store.builder().id(storeId).build(); //JPA가 완전한 Store 엔티티 객체로 관계를 인식
-//                        return PromotionStore.builder()
-//                                .promotion(promotion)
-//                                .store(store)
-//                                .build();
-//                    })
-                    .map(storeRepository::findById) // ✅ 영속 엔티티 확보
-                    .map(store -> PromotionStore.builder()
-                            .promotion(promotion)
-                            .store(store)
-                            .build())
-                    .toList();
-
-            promotionStoreRepository.saveAll(promotionStores);
-        }
-
-        if (dto.getPromotionType() == PromotionType.CATEGORY && dto.getCategory() != null) {
-            PromotionCategory link = PromotionCategory.builder()
-                    .promotion(promotion)
-                    .category(dto.getCategory())
-                    .build();
-            promotionCategoryRepository.save(link);
-        }
-
-        if (dto.getPromotionType() == PromotionType.ALL) {
-            Store hq = storeRepository.findById(1); // ✅ 본사
-            PromotionStore link = PromotionStore.builder()
-                    .promotion(promotion)
-                    .store(hq)
-                    .build();
-            promotionStoreRepository.saveAll(List.of(link));
-        }
     }
 
     public PromotionInfoListRes readList() {//비페이징용
@@ -114,22 +61,15 @@ public class PromotionService {
     }
 
     @Transactional
-    public PromotionInfoRes update(Integer id, PromotionType promotionType, ItemCategory category, String title, String contents, Double discountRate, LocalDate startDate, LocalDate endDate, PromotionStatus status,  List<Integer> storeIds,Integer itemId) {
+    public PromotionInfoRes update(Integer id, String title, String contents, LocalDate startDate, LocalDate endDate, PromotionStatus status, Integer couponId) {
         Promotion promotion = repository.findById(id);
 
-        //기본 필드 업데이트
-        if (promotion.getPromotionType() != promotionType) {
-            promotion.updatePromotionType(promotionType);
-        }
 
         if(!title.isEmpty()){
             promotion.updateTitle(title);
         }
         if(!contents.isEmpty()){
             promotion.updateContents(contents);
-        }
-        if (!discountRate.equals(promotion.getDiscountRate())) {
-            promotion.updateDiscountRate(discountRate);
         }
 
         if (!startDate.equals(promotion.getStartDate())) {
@@ -140,74 +80,18 @@ public class PromotionService {
             promotion.updateEndDate(endDate);
         }
 
-        if (!status.equals(promotion.getStatus())) {
-            promotion.updateStatus(status);
+        if (status.equals(PromotionStatus.ENDED)) {
+            promotion.updateStatus(PromotionStatus.ENDED);
+        } else {
+            // ✅ 자동 상태 재평가
+            promotion.autoUpdateStatus();
         }
 
-        if (!Objects.equals(promotion.getStatus(), status)) {
-            promotion.updateStatus(status);
+        if(!couponId.equals(promotion.getCoupon().getId())) {
+            Coupon coupon = couponRepository.findById(couponId);
+            promotion.updateCoupon(coupon);
         }
 
-//        // ✅ 자동 상태 갱신 (단, 관리자가 직접 ENDED로 바꾼 경우는 제외)
-//        if (status != PromotionStatus.ENDED) {
-//            promotion.autoUpdateStatus();
-//        }
-//
-//        if (promotionType == PromotionType.STORE) {
-//            List<Store> list = storeIds.stream().map(storeRepository::findById).toList();
-//            promotionStoreRepository.updateStores(promotion, list);
-//        }
-
-        // 타입별 링크 정리/재생성 이부분 고쳐야됨
-        // 타입별 연관 데이터 관리
-        switch (promotionType) {
-            case STORE -> {
-                // 다른 타입 링크 삭제
-                promotionItemRepository.deleteAllByPromotion(promotion);
-                promotionCategoryRepository.deleteAllByPromotion(promotion);
-
-                // 매장 재구성
-                List<Store> stores = (storeIds == null ? List.<Store>of()
-                        : storeIds.stream().map(storeRepository::findById).toList());
-                promotionStoreRepository.updateStores(promotion, stores);
-            }
-
-            case PRODUCT -> {
-                // 다른 타입 삭제
-                promotionStoreRepository.updateStores(promotion, List.of());
-                promotionCategoryRepository.deleteAllByPromotion(promotion);
-
-                // 상품 연결 재생성
-                promotionItemRepository.deleteAllByPromotion(promotion);
-                if (itemId != null) {
-                    StoreItem item = storeItemRepository.findById(itemId);
-                    promotionItemRepository.save(
-                            PromotionItem.builder().promotion(promotion).item(item).build()
-                    );
-                }
-            }
-
-            case CATEGORY -> {
-                // 다른 타입 삭제
-                promotionStoreRepository.updateStores(promotion, List.of());
-                promotionItemRepository.deleteAllByPromotion(promotion);
-
-                // 카테고리 재생성
-                promotionCategoryRepository.deleteAllByPromotion(promotion);
-                if (category != null) {
-                    promotionCategoryRepository.save(
-                            PromotionCategory.builder().promotion(promotion).category(category).build()
-                    );
-                }
-            }
-
-            case ALL -> {
-                // 모든 링크 제거
-                promotionStoreRepository.updateStores(promotion, List.of());
-                promotionItemRepository.deleteAllByPromotion(promotion);
-                promotionCategoryRepository.deleteAllByPromotion(promotion);
-            }
-        }
 
         Promotion update = repository.update(promotion);
         return PromotionInfoRes.toDto(update);
