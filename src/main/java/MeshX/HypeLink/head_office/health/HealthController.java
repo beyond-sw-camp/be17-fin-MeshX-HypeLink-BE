8a065eaaa52490d742c4797c7f48027a76ecd0b8
raccoon-coding <minsungt@naver.com>
2025-10-30 15:04:15 +0900

    [HOT] actuator 설정 완료

--- src/main/java/MeshX/HypeLink/head_office/health/HealthController.java
+++ src/main/java/MeshX/HypeLink/head_office/health/HealthController.java
@@ -6,17 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/health")
 public class HealthController {
 
     @GetMapping("/health")
-    public ResponseEntity<BaseResponse<String>> health() {
-        return ResponseEntity.status(200).body(BaseResponse.of("OK"));
+    public ResponseEntity<BaseResponse<Map<String, String>>> health() {
+        Map<String, String> status = Map.of("status", "UP");
+        return ResponseEntity.status(200).body(BaseResponse.of(status));
     }
 
     @GetMapping("/test")
-    public ResponseEntity<BaseResponse<String>> test() {
-        return ResponseEntity.status(200).body(BaseResponse.of("OK"));
+    public ResponseEntity<BaseResponse<Map<String, String>>> test() {
+        Map<String, String> status = Map.of("status", "UP");
+        return ResponseEntity.status(200).body(BaseResponse.of(status));
     }
 }
