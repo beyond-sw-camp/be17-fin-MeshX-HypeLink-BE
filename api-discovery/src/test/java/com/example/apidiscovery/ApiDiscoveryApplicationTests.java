f6442d2ab04cc8a3c60342a18634c44efa04e9a8
raccon <minsungt@naver.com>
2025-11-04 15:50:55 +0900

    [DEV] API Gateway & API Discovery(Eureka) 설치 끝. (MSA 개발하기 전에 먼저 등록하기git add .)

---
+++ api-discovery/src/test/java/com/example/apidiscovery/ApiDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apidiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
