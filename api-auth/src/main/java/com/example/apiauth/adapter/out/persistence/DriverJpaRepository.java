a2c4d67187c46e83317cc8baf66d9a88485e8bed
flionme <sunginkim142@gmail.com>
2025-11-11 17:54:51 +0900

    [DEV] 중간 저장용

--- api-auth/src/main/java/com/example/apiauth/adapter/out/persistence/DriverJpaRepository.java
+++ api-auth/src/main/java/com/example/apiauth/adapter/out/persistence/DriverJpaRepository.java
@@ -2,7 +2,6 @@ package com.example.apiauth.adapter.out.persistence;
 
 import com.example.apiauth.adapter.out.persistence.entity.DriverEntity;
 import com.example.apiauth.adapter.out.persistence.entity.MemberEntity;
-import com.example.apiauth.domain.model.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
